a0e9fe1aaf3b9e30d1909d26e1ce636eb9d52986
MuniasamyKumar <muniasamy1909@gmail.com>
2025-03-29 09:18:20 +0530

    Fix : Db connection

--- db/db.js
+++ db/db.js
@@ -1,14 +1,15 @@
 const mysql = require('mysql2');
 
 const db = mysql.createPool({
-  connectionLimit: 10,  
-  host: 'mysql.railway.internal',
-  port: 3306,
-  user: 'root',
-  password: 'YnBNDZNHCIZKyvQERUcCWjpfpWdjuiCd',
-  database: 'railway',
+ connectionLimit: 10,  
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,  
   waitForConnections: true,
-  queueLimit: 0
+  queueLimit: 0,
+  ssl: { rejectUnauthorized: false }
 });
 
 db.getConnection((err, connection) => {
@@ -16,7 +17,7 @@ db.getConnection((err, connection) => {
     console.error("⛔ Database connection failed:", err);
     return;
   }
-  console.log("✅ Database connected successfully");
+  console.log("✅ Connected to MySQL on Railway");
   connection.release();
 });
 

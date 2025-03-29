33f6688538e98fde2cad9a9f483c2b39d5f6972c
MuniasamyKumar <muniasamy1909@gmail.com>
2025-03-29 11:28:03 +0530

    Fix

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const AddressRoute = require('./router/Address')
 const db = require('./db/db')
 const app = express()
 app.use(express.json())
+const cors = require("cors");
+// app.use(cors());
 
+app.use(cors({
+    origin: "*",  // Allow all origins (use specific domains for security)
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  }));
+  
 
 
 app.use('/address',AddressRoute)
